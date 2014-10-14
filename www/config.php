6b89d8bd9fdaaf77c1f1fb702aad12cb5d3b3da4
Vipul DIvyanshu <vipuldivyanshu6.11@gmail.com>
2014-10-14 09:20:23 +0530

    config file

---
+++ www/config.php
@@ -0,0 +1,13 @@
+<?php
+ 
+// Database config variables
+define("DB_HOST", "localhost");
+define("DB_USER", "xxxxxxxxx");
+define("DB_PASSWORD", "xxxxxxx");
+define("DB_DATABASE", "gcm");
+ 
+// Google Cloud Messaging API Key
+// Place your Google API Key
+define("GOOGLE_API_KEY", "AIzaSyA81yS1VSj1WZXY_T"); 
+                           
+?>
