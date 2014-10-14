3b96893a482a39a51865b0547ab834d42d5c5376
Vipul DIvyanshu <vipuldivyanshu6.11@gmail.com>
2014-10-14 10:32:35 +0530

    Update createTable.php

--- www_2/createTable.php
+++ www_2/createTable.php
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS `gcm_users` (
   `name` varchar(50) NOT NULL,
   `email` varchar(255) NOT NULL,
   `imei` varchar(20) NOT NULL,
+  `ph_num` varchar(15) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `imei` (`imei`)
