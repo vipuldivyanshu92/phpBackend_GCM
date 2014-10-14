cbf23aebefc2f747bc9b1700dd13682667b5b31d
Vipul DIvyanshu <vipuldivyanshu6.11@gmail.com>
2014-10-14 15:50:51 +0530

    Update register.php

--- www_2/register.php
+++ www_2/register.php
@@ -8,9 +8,10 @@
            $emailUser = stripUnwantedHtmlEscape($_POST["email"]);
            $gcmRegID  = stripUnwantedHtmlEscape($_POST["regId"]); // GCM Registration ID got from device
            $imei      = stripUnwantedHtmlEscape($_POST["imei"]);
-            
+           $ph_num      = stripUnwantedHtmlEscape($_POST["ph_num"]);
+           
            // Send this message to device
-           $message = $nameUser."^".$imei."^Registered on server.";
+           $message = $nameUser."^Registered on server.";
             
            /**
             * Registering a user device in database
@@ -22,7 +23,8 @@
                 $nameUser!="" && 
                 $imei!="" && 
                 $gcmRegID!="" && 
-                $emailUser!=""
+                $emailUser!="" &&
+                $ph_num!=""
                ) {
                
               if(!isUserExisted($emailUser,$gcmRegID))
