72737b9ee1adb31f2b248eb839d63e2bc6859ebd
Vipul DIvyanshu <vipuldivyanshu6.11@gmail.com>
2014-10-14 09:44:02 +0530

    Create config.php

---
+++ www_2/config.php
@@ -0,0 +1,16 @@
+<!--?php
+     
+        /**
+         * Database config variables
+         */
+        define("DB_HOST", "localhost");
+        define("DB_USER", "XXXXXXXX");
+        define("DB_PASSWORD", "XXXXXXXX");
+        define("DB_DATABASE", "XXXXXXXX");
+         
+        /*
+         * Google Cloud Messaging API Key
+         */
+        define("GOOGLE_API_KEY", "AIzaSyA81yS1VSj1WZXY_Txxxxxxx"); // Place your Google API Key
+                                   
+    ?--> 
