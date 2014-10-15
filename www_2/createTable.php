bdce2a8f91d12e915c3b653cd54688ca8234d441
Vipul DIvyanshu <vipuldivyanshu6.11@gmail.com>
2014-10-15 15:42:46 +0530

    Update createTable.php

--- www_2/createTable.php
+++ www_2/createTable.php
@@ -1,11 +1,12 @@
-CREATE TABLE IF NOT EXISTS `gcm_users` (
+CREATE TABLE IF NOT EXISTS `giffie_users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `gcm_regid` text,
-  `name` varchar(50) NOT NULL,
+  `name` varchar(40) NOT NULL,
   `email` varchar(255) NOT NULL,
   `imei` varchar(20) NOT NULL,
   `ph_num` varchar(15) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  KEY `imei` (`imei`)
+  KEY `imei` (`imei`),
+  KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
