3341c0512b6b76c45118afe8c7db6e5398762b29
Vipul DIvyanshu <vipuldivyanshu6.11@gmail.com>
2014-10-17 19:17:34 +0530

    Update config.php

--- www_2/config.php
+++ www_2/config.php
@@ -12,5 +12,11 @@
          * Google Cloud Messaging API Key
          */
         define("GOOGLE_API_KEY", "AIzaSyDSFu-uada7tIkEj4_pKfY-hKsvOgpWKTs"); // Place your Google API Key
+/*
+*$mysql_host = "mysql8.000webhost.com";
+*$mysql_database = "a7021513_gif";
+*$mysql_user = "a7021513_gif";
+*$mysql_password = "giffie611";
+*/
                                    
     ?> 
